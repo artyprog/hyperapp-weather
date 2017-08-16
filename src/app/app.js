396ccde2323db81e2f44a9b67ed2e679aea67d88
Jack Tibbs <jdtibbs@users.noreply.github.com>
2017-08-16 08:56:01 -0500

    init functional component: city

--- src/app/app.js
+++ src/app/app.js
@@ -5,23 +5,44 @@ const {
 const hyperx = require("hyperx")
 const html = hyperx(h)
 
+const City = (getForecast) => html `
+<input class="w3-input w3-border" type="text" placeholder="City..." onkeyup=${getForecast}/>
+`;
+
+
 app({
   state: {
+    city: '',
     count: 0
   },
-  view: (state, actions) => html `
-  <main class="w3-container">
-    <h1>${state.count}</h1>
-    <button class="w3-button w3-theme" onclick=${actions.add}>+</button>
-    <button class="w3-button w3-theme" onclick=${actions.sub}>-</button>
-  </main>
-  `,
+  view: (state, actions) =>
+    html `
+    <main class="w3-container">
+      ${City(actions.getForecast)}
+      <h1>
+        ${state.count}
+      </h1>
+      <button
+        class="w3-button w3-theme"
+        onclick=${actions.add}>+</button>
+      <button
+        class="w3-button w3-theme"
+        onclick=${actions.sub}>-</button>
+    </main>`,
   actions: {
     add: state => ({
       count: state.count + 1
     }),
     sub: state => ({
       count: state.count - 1
-    })
+    }),
+    getForecast: (state, actions, {
+      target
+    }) => {
+      console.log(target.value);
+      return {
+        city: target.value
+      }
+    }
   }
 })
